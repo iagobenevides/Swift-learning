e374753c8e58fc2406d681896a51a877bdfda352
Iago Benevides Alcoforado <iagobenevidesalcoforado@MacBook-Air-de-Iago.local>
2023-06-29 22:21:14 -0300

    First day of the 100 Days of Swift

---
+++ Day 01.playground/Contents.swift
@@ -0,0 +1,7 @@
+// Variables
+
+var test = "I'm practicing my firsts 'Hello World's in Swift."
+print(test)
+
+test = "Now I can say that i'm a swift professional coder"
+print(test)
---
+++ Day 01.playground/Pages/Variables.xcplaygroundpage/Contents.swift
@@ -0,0 +1,9 @@
+// Variables
+
+var test = "I'm practicing my firsts 'Hello World's in Swift."
+print(test)
+
+print() // Writing just a 'print' cause it by itself has a "\n" at the end
+
+test = "Now I can say that i'm a swift professional coder"
+print(test)
