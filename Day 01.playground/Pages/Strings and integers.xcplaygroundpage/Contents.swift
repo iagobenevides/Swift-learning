5dc1d15c54a79d1ff8d1118ba7fec7289c98e55b
Iago Benevides Alcoforado <iagoalcoforado@hotmail.com>
2023-07-02 22:45:38 -0300

    Multi line strings subject, in a complement of the first day of the 100 days of swift program

--- Day 01.playground/Pages/Strings and integers.xcplaygroundpage/Contents.swift
+++ Day 01.playground/Pages/Strings and integers.xcplaygroundpage/Contents.swift
@@ -7,3 +7,9 @@ var int = 21
 var int2 = 21.000
 
 var int3 = 21.001
+
+// Multi-lines strings
+
+let result = "This is a phrase that you can call it 'String'\nand the result is this."
+
+print(result)
