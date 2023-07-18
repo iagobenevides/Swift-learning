3c7b89b1b7e4a72fd92572a47545a3c5ce108903
Iago Benevides Alcoforado <iagoalcoforado@hotmail.com>
2023-07-18 19:25:31 -0300

    Enums raw values.

---
+++ Day 02.playground/Pages/10 Enum raw values.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+// Enum raw values
+
+enum planetas: Int {
+    case mercurio = 1
+    case venus
+    case terra
+    case marte
+}
+
+// You can also do something like this:
+
+let terra = planetas(rawValue:3)
+
