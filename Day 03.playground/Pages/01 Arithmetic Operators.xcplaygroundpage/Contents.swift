ce5792b556a2f477a1f4c9cd8c342e12baa5d214
Iago Benevides Alcoforado <iagoalcoforado@hotmail.com>
2023-07-21 22:19:09 -0300

    Class of mathematics

---
+++ Day 03.playground/Pages/01 Arithmetic Operators.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+// Arithmetic Operators
+
+import UIKit
+
+let numX = 7
+let numY = 3
+
+let soma = numX + numY
+print(soma)
+let subtracao = numX - numY
+
+let multiplicacao = numX * numY
+let divisao = numX / numY
+
+let rest = numX % numY
